56e68cf9605c19624f67aaa63d5d6bfd21f3d56f
vinicfrancisco <vini.ibi@gmail.com>
2020-04-23 15:17:37 -0300

    sign in page layout

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,17 +1,63 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logoImage from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccount,
+  CreateAccountText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImage} />
+    <KeyboardAvoidingView
+      enabled
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Image source={logoImage} />
 
-      <Title>Faça seu logon</Title>
-    </Container>
+          <View>
+            <Title>Faça seu logon</Title>
+          </View>
+
+          <Input name="email" icon="mail" placeholder="E-mail" />
+
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button onPress={() => console.log('clicou')}>Entrar</Button>
+
+          <ForgotPassword onPress={() => console.log('click')}>
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+      </ScrollView>
+
+      <CreateAccount onPress={() => console.log('click')}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+
+        <CreateAccountText>Criar uma conta</CreateAccountText>
+      </CreateAccount>
+    </KeyboardAvoidingView>
   );
 };
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   align-items: center;
   flex: 1;
   justify-content: center;
+  padding: 0 32px;
 `;
 
 export const Title = styled.Text`
@@ -11,4 +13,35 @@ export const Title = styled.Text`
   font-family: 'RobotoSlab-Medium';
   font-size: 24px;
   margin: 64px 0 24px;
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Regular';
+  font-size: 16px;
+`;
+
+export const CreateAccount = styled.TouchableOpacity`
+  align-items: center;
+  background: #312e38;
+  border-top-width: 1px;
+  border-top-color: #232129;
+  bottom: 0;
+  flex-direction: row;
+  justify-content: center;
+  left: 0;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+  position: absolute;
+  right: 0;
+`;
+
+export const CreateAccountText = styled.Text`
+  color: #ff9000;
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+  margin-left: 16px;
 `;
