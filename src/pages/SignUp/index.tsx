7bf5bbdc81f85864a674463a626dfd115cfee0c0
vinicfrancisco <vini.ibi@gmail.com>
2020-04-23 15:26:48 -0300

    create signup layout

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import logoImage from '../../assets/logo.png';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+
+  return (
+    <KeyboardAvoidingView
+      enabled
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Image source={logoImage} />
+
+          <View>
+            <Title>Crie sua conta</Title>
+          </View>
+
+          <Input name="name" icon="user" placeholder="Nome" />
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button onPress={() => console.log('clicou')}>Entrar</Button>
+        </Container>
+      </ScrollView>
+
+      <BackToSignIn onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+
+        <BackToSignInText>Voltar para logon</BackToSignInText>
+      </BackToSignIn>
+    </KeyboardAvoidingView>
+  );
 };
 
 export default SignUp;
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,3 +1,38 @@
+import { Platform } from 'react-native';
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+  padding: 0 32px ${Platform.OS === 'ios' ? 40 : 150}px;
+`;
+
+export const Title = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 24px;
+  margin: 64px 0 24px;
+`;
+
+export const BackToSignIn = styled.TouchableOpacity`
+  align-items: center;
+  background: #312e38;
+  border-top-width: 1px;
+  border-top-color: #232129;
+  bottom: 0;
+  flex-direction: row;
+  justify-content: center;
+  left: 0;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+  position: absolute;
+  right: 0;
+`;
+
+export const BackToSignInText = styled.Text`
+  color: #fff;
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+  margin-left: 16px;
+`;
