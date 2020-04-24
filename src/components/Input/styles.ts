2bcd148f8d4a1afe18cf0ec702b78d3460f00375
vinicfrancisco <vini.ibi@gmail.com>
2020-04-24 09:13:08 -0300

    added error handling on input component

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,21 +1,34 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
 interface ContainerProps {
   isFocused: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.View<ContainerProps>`
   align-items: center;
   background: #232129;
   border-radius: 10px;
-  border-color: ${(props) => (props.isFocused ? '#ff9000' : '#232129')};
+  border-color: #232129;
   border-width: 2px;
   flex-direction: row;
   height: 60px;
   margin-bottom: 8px;
   padding: 0 16px;
   width: 100%;
+
+  ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      border-color: #ff9000;
+    `}
 `;
 
 export const TextInput = styled.TextInput`
