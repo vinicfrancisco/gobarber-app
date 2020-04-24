5a7ab2935cd3fc22e3f877c73bdd6adfc25d35d6
vinicfrancisco <vini.ibi@gmail.com>
2020-04-24 10:18:52 -0300

    added app provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './routes';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
   <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
 
-    <Routes />
+    <AppProvider>
+      <Routes />
+    </AppProvider>
   </NavigationContainer>
 );
 
